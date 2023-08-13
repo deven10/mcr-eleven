098942e8062811a1c1703e89a0a76457332dbea1
Deven Umrania <umraniadeven10@gmail.com>
2023-08-13 15:02:11 +0530

    fix

--- src/context/AddNewMovieModal.js
+++ src/context/AddNewMovieModal.js
@@ -11,11 +11,11 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: 500,
   bgcolor: "background.paper",
   border: "1px solid #000",
   boxShadow: 24,
-  p: 4,
+  p: 2,
 };
 
 export const AddNewMovieModal = () => {
